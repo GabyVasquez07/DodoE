a4933afdb6549e3f60720247fd561fb111bd9a23
sant1ago-quito <santiagokito2000@gmail.com>
2025-04-24 11:40:11 -0600

    Actualización

--- app/Models/ModelEventos.php
+++ app/Models/ModelEventos.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class ModelEventos extends Model
 {
-    //
+    protected $table = 'tbl_eventos';
+    protected $fillable = [
+        'fk_usuario',
+        'nombre_evento',
+        'descripcion',
+        'fecha_evento',
+        'hora_evento',
+        'ubicacion',
+        'estado',
+        'asistentes'
+    ];
+
+    public function Eventos()
+    {
+        return $this->belongsTo(ModelUsuario::class);
+    }
 }
