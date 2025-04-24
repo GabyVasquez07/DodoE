6e8130bc31482fb54f976f3c94ac0a5d56c7bebb
sant1ago-quito <santiagokito2000@gmail.com>
2025-04-24 11:32:46 -0600

    actualizacion

--- app/Models/ModelUsuario.php
+++ app/Models/ModelUsuario.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class ModelUsuario extends Model
 {
-    //
+    protected $table = 'tbl_usuarios';
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'email',
+        'password',
+        'telefono',
+        'direccion',
+        'rol_id',
+    ];
+
+    public function Eventos()
+    {
+        return $this->hasMany(ModelEventos::class);
+    }
+   
 }
